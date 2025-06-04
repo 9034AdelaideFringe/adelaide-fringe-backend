63557c1520fb80ab3a951163aa1ccd37fb81b6ad
clx <zzbn258@gmail.com>
2025-06-04 23:09:29 +0930

    fix cors

--- include/middlewares/AdelaideCORS.h
+++ include/middlewares/AdelaideCORS.h
@@ -17,10 +17,11 @@ struct AdelaideCORS
 
   void after_handle(crow::request &req, crow::response &res, context &ctx)
   {
-    auto &origin = req.get_header_value("Origin");
-    string originFill = origin.empty() ? "null" : origin;
-    res.set_header("Access-Control-Allow-Origin", move(originFill));
+    string origin = req.get_header_value("Origin");
+    string originFill = (origin.empty() ? "null" : origin);
+    res.set_header("Access-Control-Allow-Origin", originFill);
     res.set_header("Access-Control-Allow-Methods", "*");
+    res.set_header("Access-Control-Allow-Headers", "Content-Type");
     res.set_header("Access-Control-Allow-Credentials", "true");
 
   }
