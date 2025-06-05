746760f4e04109c6a9df8e4a9206c9b7c66dbc2e
Jiang Tang <166135370+cnmzsjbz199328@users.noreply.github.com>
2025-06-05 23:09:47 +0930

    Update Singleton.h

--- include/utils/Singleton.h
+++ include/utils/Singleton.h
@@ -1,9 +1,10 @@
 #pragma once
 
 #include "crow.h"
-#include "middlewares/AdelaideCORS.h"
+#include "crow/middlewares/cors.h" // 包含 Crow 内置的 CORS 中间件头文件
 #include "middlewares/AuthMiddleware.h"
 
-crow::App<AdelaideCORS, AuthMiddleware>& getApp();
+// 将 App 的模板参数改回 crow::CORSHandler
+crow::App<crow::CORSHandler, AuthMiddleware>& getApp();
 
 crow::Blueprint& getBlueprint();
